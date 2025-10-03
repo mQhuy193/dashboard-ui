a4c19ae4161d3a4de4c7b776d4a80fd3ed8c23ec
mQhuy193 <mQhuy1903@gmail.com>
2025-10-03 09:20:57 +0700

    them action

--- src/components/Layout/Header/index.jsx
+++ src/components/Layout/Header/index.jsx
@@ -1,7 +1,9 @@
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { faBell } from '@fortawesome/free-regular-svg-icons';
 
+import images from '@/assets/images';
 import styles from './Header.module.scss';
 import Button from '@/components/Button';
 
@@ -14,14 +16,17 @@ function Header() {
                 <h2 className={cx('title')}>Dashboard tổng quan</h2>
                 <p className={cx('description')}>Chào mừng bạn quay trở lại!</p>
             </div>
-            <div className="action">
+            <div className={cx('actions')}>
                 {/* <Button primary>Sign in</Button>
                 <Button outline>Sign up</Button> */}
                 <div className={cx('search')}>
-                    <input type="text" placeholder="Tìm kiếm..." />
+                    <input type="text" placeholder="Tìm kiếm..." className={cx('input')} />
                     <FontAwesomeIcon icon={faMagnifyingGlass} />
                 </div>
-                <img className={cx('avatar')} alt="avatar" />
+                <div className={cx('notification')}>
+                    <FontAwesomeIcon icon={faBell} />
+                </div>
+                <img className={cx('avatar')} alt="avatar" src={images.avatar} />
             </div>
         </header>
     );
