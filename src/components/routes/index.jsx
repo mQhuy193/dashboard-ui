d98d1798e8de5f770dc4896324b75abd05a7bed8
mQhuy193 <mQhuy1903@gmail.com>
2025-10-20 19:52:14 +0700

    remove some import line

--- src/components/routes/index.jsx
+++ src/components/routes/index.jsx
@@ -1,8 +1,5 @@
 import Dashboard from '@/components/pages/Dashboard';
-import ContactsManage from '@/components/pages/ContactsManage';
 import PostsManage from '@/components/pages/PostsManage';
-import Setting from '@/components/pages/Setting';
-import UsersManage from '@/components/pages/UsersManage';
 
 const publicRoutes = [
     { path: '/', component: Dashboard },
